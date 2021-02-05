ba7d5145aa941c483288463c0adff40735e9fde9
Andrei <vrasmas_vasy@yahoo.com>
2021-02-05 13:12:40 +0000

    Add place form completed

--- src/shared/components/UIElements/Map.js
+++ src/shared/components/UIElements/Map.js
@@ -1,20 +1,22 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect } from "react";
 
-import './Map.css';
+import "./Map.css";
 
-const Map = props => {
+const Map = (props) => {
   const mapRef = useRef();
-  
+
   const { center, zoom } = props;
 
   useEffect(() => {
+    // Generate map using the coordinates
     const map = new window.google.maps.Map(mapRef.current, {
       center: center,
-      zoom: zoom
+      zoom: zoom,
     });
-  
+
+    // Generate marker on the map
     new window.google.maps.Marker({ position: center, map: map });
-  }, [center, zoom]);  
+  }, [center, zoom]);
 
   return (
     <div
